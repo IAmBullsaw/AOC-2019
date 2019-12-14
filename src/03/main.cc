a5c66b1baa392a8db97a11be1689748ecf0f0207
Oskar Jansson <oskja067@student.liu.se>
2019-12-14 19:45:42 +0100

    updates: day 3

--- src/03/main.cc
+++ src/03/main.cc
@@ -20,13 +20,53 @@ int manhattan_distance(pair<int,int> p1, pair<int,int> p2) {
   return x + y;
 }
 
+
+void print_grid(vector<vector<char>> const& grid) {
+  for (auto const& row : grid) {
+    for (auto const& c : row) {
+      cout << c;
+    }
+    cout << "\n";
+  }
+}
+
+vector<vector<char>> create_grid(vector<vector<pair<int,int>>> const& wires) {
+  int max_x_size{0};
+  int max_y_size{0};
+  for (auto const& wire : wires) {
+    for (auto const& p: wire) {
+      if (p.first > max_x_size) max_x_size = p.first;
+      if (p.second > max_y_size) max_y_size = p.second;
+    }
+  }
+  vector<vector<char>> grid(max_y_size + 4, vector<char>(max_x_size + 4, '.'));
+  return grid;
+}
+
+vector<vector<char>> add_wire_to_grid(vector<vector<char>> grid, vector<pair<int,int>> const& wire) {
+  for (auto const& p : wire) {
+    auto point = grid[p.first][p.second];
+    if (point == '*') grid[p.first][p.second] = 'x';
+    else if (point == '.') grid[p.first][p.second] = '*';
+    else {cout << "AAAAAAAAAAAAAAAAAAAAAAA";}
+  }
+  return grid;
+}
+
+
+void print_wire(vector<pair<int,int>> wire) {
+  for (auto const& w : wire) {
+    cout << "(" << w.first  << ":" << w.second << ") ";
+  }
+  cout << endl;
+}
+
 int main(int , char** argv) {
   fstream f{argv[1]};
   string buffer{""};
 
   vector<vector<pair<int, int>>> wires{};
   while (getline(f, buffer)) {
-    cout << "Wire: start" << endl;
     stringstream ss{buffer};
     int x{0};
     int y{0};
@@ -45,25 +85,28 @@ int main(int , char** argv) {
     }
     sort(begin(wire),end(wire));
     wires.push_back(wire);
-    cout << "Wire: done" << endl;
   }
 
-  vector<pair<int,int>> crossings{wires.at(0)};
+  auto grid = create_grid(wires);
+  for( auto const& wire: wires) grid = add_wire_to_grid(grid,wire);
+  // print_grid(grid);
 
-  // Union of all wires
-  for (auto wire: wires) {
-    cout << "Wires: union" << endl;
-    vector<pair<int,int>> tmp{};
-    set_union(begin(crossings), end(crossings),
-              begin(wire), end(wire),
-              back_inserter(tmp));
-    crossings = tmp;
-    cout << "Wires: union done" << endl;
+  vector<pair<int,int>> crossings{};
+
+  for (int i{0}; static_cast<unsigned>(i) < wires.size() - 1; ++i) {
+    set_intersection(begin(wires[i]), end(wires[i]),
+                     begin(wires[i + 1]), end(wires[i + 1]),
+                     back_inserter(crossings));
   }
 
   // Unique of all crossings of wires
   auto last = unique(begin(crossings),end(crossings));
   crossings.erase(last,end(crossings));
+
+  grid = create_grid(wires);
+  grid = add_wire_to_grid(grid, crossings);
+  // print_grid(grid);
+
 
   // manhattan distance
   pair<int,int> closest{crossings.at(0)};
