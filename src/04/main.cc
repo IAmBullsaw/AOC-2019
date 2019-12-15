1fa07b790e88a6df5d673ac03a6d4a09c7049ad0
Oskar Jansson <oskja067@student.liu.se>
2019-12-15 14:39:32 +0100

    adds: day 4.2 done

--- src/04/main.cc
+++ src/04/main.cc
@@ -12,11 +12,38 @@ bool never_decrease(int const& password) {
   auto str_password = to_string(password);
   auto found = adjacent_find(begin(str_password), end(str_password), greater<int>());
   return found == end(str_password);
+}
 
+bool two_adjacent_are_the_same(int const& password) {
+  auto str_password = to_string(password);
+  auto found = adjacent_find(begin(str_password), end(str_password), equal_to<int>());
+  return found != end(str_password);
+}
+
+bool two_adjacent_are_the_same_and_not_in_a_bigger_block(int const& password) {
+  auto str_password = to_string(password);
+  auto it = begin(str_password);
+  auto cur = *it;
+  auto count{1};
+  vector<int> counts{};
+  while (it != end(str_password)) {
+    it = next(it);
+    if (cur == *it) {
+      count++;
+    } else {
+      if (count > 1) {
+        counts.push_back(count);
+      }
+      count = 1;
+      cur = *it;
+    }
+  }
+
+  auto answer = any_of(begin(counts), end(counts), [](auto const& a) { return a == 2;});
+  return answer;
 }
 
 int main(int, char** argv) {
-
   fstream f{argv[1]};
   string buffer{""};
   getline(f, buffer);
@@ -24,8 +51,12 @@ int main(int, char** argv) {
   int minimum,maximum;
   char line;
   ss >> minimum >> line >> maximum;
-  for (int password{minimum}; password < maximum + 1; password++ ) {
-    if (never_decrease(password)) cout << password << " never decreases" << "\n";
 
+  long long count{0};
+  for (int password{minimum}; password < maximum + 1; password++ ) {
+    if (never_decrease(password) && two_adjacent_are_the_same_and_not_in_a_bigger_block(password)) {
+      count++;
+    }
   }
+  cout << "Possible combinations: " << count << "\n";
 }
