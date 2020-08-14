1cbfbc759f324a424e8b666952810546f60fad20
Oskar Jansson <bullsaw1@gmail.com>
2020-08-14 16:25:09 +0200

    Update 01.go

--- go/01.go
+++ go/01.go
@@ -1,15 +1,39 @@
 package main
 
 import (
-	"fmt"
-	"math"
+    "bufio"
+    "fmt"
+    "os"
+    "strconv"
+    "errors"
+    "math"
 )
 
-func calculate_required_fuel(mass float64) float64 {
-	return math.Floor(mass/3) - 2
-}
-
 func main() {
-	result := calculate_required_fuel(12)
-	fmt.Printf("result %f", result)
+    scanner := bufio.NewScanner(os.Stdin)
+    text := ""
+    mass := 0
+    err := errors.New("not an error")
+    total_fuel := 0
+    for scanner.Scan() {
+        text = scanner.Text()
+        mass, err = strconv.Atoi(text)
+        if err != nil {
+         fmt.Println(err)
+         break;
+        }
+
+        module_fuel := 0
+        for {
+            mass = int(math.Floor(float64(mass)/3) - 2)
+            if mass > 0 {
+                module_fuel += mass
+            } else {
+                break;
+            }
+        }
+
+        total_fuel += module_fuel
+    }
+    fmt.Println("total fuel:", total_fuel)
 }
